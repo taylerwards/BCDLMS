12f710ddc8fb9f253506a9967c32db8bac61d5f8
taylerwards <walter.lad@deped.gov.ph>
2024-06-27 23:41:14 +0800

    first commit

---
+++ resources/views/livewire/user-dashboard.blade.php
@@ -0,0 +1,5 @@
+<div>
+    {{-- Do your work, then step back. --}}
+
+    <h2>This your dashboard</h2>
+</div>
---
+++ resources/views/livewire/user-login.blade.php
@@ -0,0 +1,27 @@
+<div>
+    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+
+    <div class="bg-white h-screen w-screen p-1">
+
+
+        <div class="w-96 h-80 bg-gray-900 mx-auto mt-20 rounded-lg p-5">
+            <div class="flex items-center justify-center mt-6">
+                <img src="div.png" alt="" class="w-20 h-20 ">
+            </div>
+            <p class="text-md text-2xl font-bold mt-4 text-center text-white">BCD-LMIS</p>
+
+
+
+            <div class="mt-8 flex items-center justify-center">
+                <a wire:navigate href="/dashboard"><img src="/google.png" alt="" class="w-60 h-18"></a>
+            </div>
+
+
+
+
+        </div>
+
+    </div>
+
+
+</div>
